de9047f2bda553113069c0530c253d42b980362e
Kynan Stewart Hughes <mail@kynan.net.au>
2021-10-24 21:05:10 +1100

    Trying to get this file deployed to GitHub pages.

--- web_modules/htm/preact.js
+++ web_modules/htm/preact.js
@@ -1,22 +1,40 @@
-import { p as preact_module } from '../common/preact.module-c2015350.js';
-import require$$0 from '../htm.js';
+// Nothing.
+import { p as preact_module } from "../common/preact.module-c2015350.js";
+import require$$0 from "../htm.js";
 
 function createCommonjsModule(fn, basedir, module) {
-	return module = {
-		path: basedir,
-		exports: {},
-		require: function (path, base) {
-			return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-		}
-	}, fn(module, module.exports), module.exports;
+  return (
+    (module = {
+      path: basedir,
+      exports: {},
+      require: function (path, base) {
+        return commonjsRequire(
+          path,
+          base === undefined || base === null ? module.path : base,
+        );
+      },
+    }),
+    fn(module, module.exports),
+    module.exports
+  );
 }
 
-function commonjsRequire () {
-	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
+function commonjsRequire() {
+  throw new Error(
+    "Dynamic requires are not currently supported by @rollup/plugin-commonjs",
+  );
 }
 
 var preact = createCommonjsModule(function (module, exports) {
-var e,t=((e=require$$0)&&"object"==typeof e&&"default"in e?e.default:e).bind(preact_module.h);exports.h=preact_module.h,exports.render=preact_module.render,exports.Component=preact_module.Component,exports.html=t;
+  var e,
+    t = ((e = require$$0) && "object" == typeof e && "default" in e
+      ? e.default
+      : e
+    ).bind(preact_module.h);
+  (exports.h = preact_module.h),
+    (exports.render = preact_module.render),
+    (exports.Component = preact_module.Component),
+    (exports.html = t);
 });
 
 var Component = preact.Component;
