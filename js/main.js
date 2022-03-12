557680d3c7bd36c92806d191222c30a566fba564
Kynan Stewart Hughes <mail@kynan.net.au>
2022-03-12 20:56:31 +1100

    Checkout out the /js/main.js to get previous version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
+// @flow
 import { h, hydrate, render } from "../web_modules/preact.js";
 import App from "./App.js";
 import { html } from "../web_modules/htm/preact.js";
 
 hydrate(html` <${App} /> `, document.getElementById("goodthing"));
+
+// Doesn't work on iPhone ~ https://caniuse.com/#feat=fullscreen
+// Plus we only want fullscreen on touch devices
+// $FlowFixMe
+if (Modernizr.hasEvent("touchend") && screenfull.isEnabled) {
+  // $FlowFixMe
+  document.getElementById("goodthing").addEventListener(
+    "touchend",
+    () => {
+      screenfull.request();
+    },
+    { once: true },
+  );
+}
+
+// Fix for iOS not handling viewport height the same way as everyone else
+if (
+  typeof window !== "undefined" &&
+  !!navigator.platform &&
+  /iPad|iPhone|iPod/.test(navigator.platform)
+) {
+  // it's probably an iPhone
+  window.onresize = function () {
+    // $FlowFixMe
+    document.body.style.height = window.innerHeight + "px";
+    // $FlowFixMe
+    document.body.parentNode.style.height = window.innerHeight + "px";
+    // $FlowFixMe
+    document.getElementById("goodthing").style.height =
+      window.innerHeight + "px";
+  };
+  window.onresize(); // called to initially set the height.
+}
+
+// Cookie redirect
+const name = "shift314-decision-cards";
+const cDecoded = decodeURIComponent(document.cookie);
+const cArr = cDecoded.split("; ");
+let relocate = true;
+cArr.forEach((val) => {
+  if (val.indexOf(name) === 0) {
+    relocate = false;
+  }
+});
+if (relocate === true) {
+  window.location.assign("https://shift314.com/decisioncards/#access-cards");
+}
